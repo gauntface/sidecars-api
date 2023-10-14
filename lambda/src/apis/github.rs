41ac104531afd7e9ef25c16b92bf2bf7654b6139
Matt Gaunt-Seo <matt@gaunt.dev>
2023-10-14 13:45:04 -0700

    Add start of GitHub app support

---
+++ lambda/src/apis/github.rs
@@ -0,0 +1,82 @@
+extern crate jsonwebtoken;
+extern crate reqwest;
+extern crate serde;
+extern crate serde_json;
+
+use jsonwebtoken::{encode, Algorithm, Header};
+use reqwest::header::{HeaderMap, HeaderValue};
+use serde::{Deserialize, Serialize};
+use serde_json::json;
+use std::env;
+
+#[derive(Debug, Serialize, Deserialize)]
+pub(crate) struct Permissions {
+    pub metadata: Option<String>,
+    pub contents: Option<String>,
+    pub issues: Option<String>,
+    pub single_file: Option<String>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub(crate) struct GitHubApp {
+    pub id: u32,
+    pub node_id: String,
+    pub slug: String,
+    pub name: String,
+    pub description: String,
+    pub external_url: String,
+    pub html_url: String,
+    pub permissions: Permissions,
+    pub events: Vec<String>,
+}
+
+pub(crate) fn get_app() -> Result<GitHubApp, Box<dyn std::error::Error>> {
+    // Set your GitHub App's App ID and Private Key
+    let app_id =
+        env::var("GITHUB_APP_ID").expect("Environment variable GITHUB_APP_ID is not defined.");
+    let private_key = env::var("GITHUB_APP_PRIVATE_KEY")
+        .expect("Environment variable GITHUB_APP_PRIVATE_KEY is not defined.");
+
+    // Generate a JSON Web Token (JWT) for GitHub App authentication
+    let token = encode(
+        &Header::new(Algorithm::RS256),
+        &json!({
+                "iat": chrono::Utc::now().timestamp(),
+                "exp": chrono::Utc::now().timestamp() + 600, // Set expiration time (10 minutes)
+                "iss": app_id,
+        }),
+        &jsonwebtoken::EncodingKey::from_rsa_pem(private_key.as_bytes())?,
+    )?;
+
+    // Create headers with the JWT for GitHub App authentication
+    let mut headers = HeaderMap::new();
+    headers.insert(
+        "Authorization",
+        HeaderValue::from_str(&format!("Bearer {}", token))?,
+    );
+    headers.insert("User-Agent", HeaderValue::from_str("sidecar")?);
+    headers.insert(
+        "Accept",
+        HeaderValue::from_str("application/vnd.github+json")?,
+    );
+
+    // Send a POST request to the GitHub GraphQL API
+    let client = reqwest::blocking::Client::new();
+    let response = client
+        .get("https://api.github.com/app")
+        .headers(headers)
+        .send()?;
+
+    // Check if the request was successful
+    if response.status().is_success() {
+        // Deserialize the response JSON
+        let github_app: GitHubApp = response.json()?;
+        return Ok(github_app);
+    }
+
+    Err(Box::from(format!(
+        "GitHub App request failed: ({}) {:?}",
+        response.status(),
+        response.text()
+    )))
+}
